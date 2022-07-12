9f8b1ccd6bb950a6c29a30544eb215153f948603
LouisPhan <phanquyen7799@gmail.com>
2022-07-12 14:30:23 +0700

    setup_file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div
+      className='flex h-20 max-w-sm justify-center items-center bg-orange-200 
+    rounded-2xl m-2 hover:bg-yellow-900 transition-all'
+    >
+      <h1 className='hover:text-gray-50 flex-1 text-center py-6 font-medium animate-spin'>
+        Hello React
+      </h1>
+    </div>
+  );
+}
+
+export default App;
